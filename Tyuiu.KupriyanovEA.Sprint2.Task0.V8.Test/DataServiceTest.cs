e924115334ca5e4791ff5664cdf9147f40be1b59
Tyuiu.KupriyanovEA@outlook.com <Tyuiu.KupriyanovEA@outlook.com>
2023-10-02 17:15:00 +0500

    Task0

--- Tyuiu.KupriyanovEA.Sprint2.Task0.V8.Test/DataServiceTest.cs
+++ Tyuiu.KupriyanovEA.Sprint2.Task0.V8.Test/DataServiceTest.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
+using Tyuiu.KupriyanovEA.Sprint2.Task0.V8.Lib;
+
 namespace Tyuiu.KupriyanovEA.Sprint2.Task0.V8.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidGetCompareOperations()
         {
+            DataService ds = new DataService();
+            int x = 185;
+            int y = 251;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { true, false, true, false, true, false };
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
--- Tyuiu.KupriyanovEA.Sprint2.Task0.V8/Program.cs
+++ Tyuiu.KupriyanovEA.Sprint2.Task0.V8/Program.cs
@@ -4,12 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tyuiu.KupriyanovEA.Sprint2.Task0.V8.Lib;
+
 namespace Tyuiu.KupriyanovEA.Sprint2.Task0.V8
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил Куприянов Е. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Куприянов Евгений Александрович | АСОиУб-23-2                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+            Console.WriteLine("* (True, False, True, False, True, False), при x = 185, y = 251           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 185;
+            int y = 251;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i=0; i<6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
