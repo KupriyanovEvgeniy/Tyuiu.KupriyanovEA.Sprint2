1b6ebd71169bde5bce70c66032720ddbededb5c1
Tyuiu.KupriyanovEA@outlook.com <Tyuiu.KupriyanovEA@outlook.com>
2023-10-02 16:35:31 +0500

    Add project files.

---
+++ Tyuiu.KupriyanovEA.Sprint2.Task0.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KupriyanovEA.Sprint2.Task0.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KupriyanovEA.Sprint2.Task0.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fa882bab-ef86-435b-97da-6ee11f4a540d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
